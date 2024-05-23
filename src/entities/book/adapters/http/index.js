bf5fcedcb6d6fac1a125793f7324abdc599e64b4
mañana <ericalaa@ymail.com>
2024-05-23 13:55:31 +0200

    Register

--- src/entities/book/adapters/http/index.js
+++ src/entities/book/adapters/http/index.js
@@ -13,12 +13,10 @@ router.get('/', asyncHandler(async (req, res) => {
 router.post('/register', asyncHandler(async (req, res) => {
   try {
     const { email, password } = req.body;
-console.log('*********',email)
     const existingUser = await Model.get({ email });
     if (existingUser.length>0) {
       return res.status(400).json({ message: 'User already exists' });
     }
-
     await Model.create({ email, password });
     res.status(201).json({ message: 'User registered successfully' });
   } catch (error) {
@@ -26,7 +24,39 @@ console.log('*********',email)
     res.status(500).json({ message: 'Internal Server Error' });
   }
 }));
+router.post('/signin', asyncHandler(async (req, res) => {
+  const { email, password } = req.body;
 
+  try {
+      const user = await Model.get({ email});
+      if (user.length===0) {
+          return res.status(400).json({ message: 'Invalid email or password' });
+        }
+    const userpassword = await Model.get({ password }); 
+    if (userpassword.length===0) {
+      return res.status(400).json({ message: 'Invalid email or password' });
+    }
+    // console.log(user, "Dandandajna");
+    // Return user data on successful sign-in
+    res.status(200).json(user);
+  } catch (error) {
+    console.error('Error signing in user:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}));
+
+router.get('/users', asyncHandler(async (req, res) => {
+  try {
+    // Retrieve all users from the database
+    const users = await Model.get();
+    
+    const userList = users.map(user => ({ id: user.id, email: user.email }));
+    res.status(200).json(userList);
+  } catch (error) {
+    console.error('Error retrieving users:', error);
+    res.status(500).json({ message: 'Internal Server Error' });
+  }
+}));
 
 
 export default (app, entityUrl) => app.use(entityUrl, router);
--- src/entities/book/model/schema.js
+++ src/entities/book/model/schema.js
@@ -2,6 +2,6 @@ import { db, DataTypes } from '@Application/database';
 
 export default db.define('book', {
 	email: DataTypes.STRING,
-	auth0Id: DataTypes.STRING,
+	password: DataTypes.STRING,
 });
 
